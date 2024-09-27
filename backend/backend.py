28ec8b6827a44d3273c3a00bec21778378586e9f
TURRA7 <turra777@mail.ru>
2024-09-27 12:17:07 +0400

    update doc backend

--- backend/backend.py
+++ backend/backend.py
@@ -13,7 +13,7 @@ async def get_html(url: str):
 
     returns:
 
-        Возвращает словарь с данными сайта.
+        Возвращает словарь с данными сайта(МВИДЕО).
     """
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
