7337a4a6f21ef7d2e7a6e80cd1d302dc6144c59d
TURRA7 <turra777@mail.ru>
2024-09-28 19:06:23 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Подключение для базы данных PostgreSQL
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 
+# Настройки приложения
 SECRET_KEY = os.environ.get("SECRET_KEY")
